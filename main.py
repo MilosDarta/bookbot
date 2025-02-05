ca77ed3d8f17bd842ee4074c461c82aa24c886cf
github_username_here <email@example.com>
2025-02-05 10:59:32 +0500

    add frequency counter function

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ def read_write_file(path_to_file):
     print(file_contents)
     print(len(file_contents.split()))
 
+def char_count(path_to_file):
+    with open(path_to_file) as f:
+        file_contents_lower = f.read().lower()
+#    freq = {c: file_contents_lower.count(c) for c in file_contents_lower}
+    freq = {}
+    for c in file_contents_lower:
+        if c not in freq:
+            freq[c] = 0
+        freq[c] += 1
+    return freq
+
 def main():
     path_to_file = "books/frankenstein.txt"
-    read_write_file(path_to_file)
+    print(char_count(path_to_file))
 
 if __name__ == '__main__':
     main()
